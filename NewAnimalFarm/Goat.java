42097ddab5abbf54e3c580c14b147e31ee723664
efecetinkaya <96293596+efecetinkaya@users.noreply.github.com>
2023-09-08 16:33:14 +0300

    Add files via upload

---
+++ NewAnimalFarm/Goat.java
@@ -0,0 +1,31 @@
+package new_animal_farm;
+
+public class Goat extends Animal{
+
+	String type = "Goat";
+	
+	public Goat(int age, String gender) {
+		super(age, gender);
+	}
+	
+	
+	
+	@Override
+	void obtainProduct() {
+		System.out.println("Keçi ürün üretti.");
+		//currentAnimal listesindeki hayvanlara uygulanacak.
+		//yemi azaltıp parayı arttıracak.
+		//button'a bağlı olacak.
+	}
+
+	@Override
+	int getProductTime() {
+		// TODO Auto-generated method stub
+		return 10000;
+	}
+	@Override
+	String getType() {
+		// TODO Auto-generated method stub
+		return type;
+	}
+}
